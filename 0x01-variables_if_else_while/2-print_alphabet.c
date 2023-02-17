27a7e209b193ee3dd264e1b6ee1beb1a93d37c75
Turbo90092 <cliveturbo2@gmail.com>
2023-02-17 11:17:24 -0800

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char lc = 'a';
+
+	for (lc = 'a'; lc < 'a'; ++lc)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
