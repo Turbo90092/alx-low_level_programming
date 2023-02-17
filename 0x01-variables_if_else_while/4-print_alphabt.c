9c5577ae70b9bd7ce85e059027999d23e8838f7a
Turbo90092 <cliveturbo2@gmail.com>
2023-02-17 14:35:57 -0800

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters except q and e
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch == 'e' || ch == 'q')
+	{
+		continue;
+	}
+	putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
