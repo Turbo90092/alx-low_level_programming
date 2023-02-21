c1dac9836113d5fa87238351882793fbce9552ca
Turbo90092 <cliveturbo2@gmail.com>
2023-02-21 09:33:47 -0800

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-#include "main.h"
 
 /**
  * main - prints _putchar
@@ -12,14 +11,13 @@
 
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	retur(0);
 }	
