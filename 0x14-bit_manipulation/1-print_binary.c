992db4f95d3c4078c808416e2b6b00d8b34bd599
Turbo90092 <cliveturbo@gmail.com>
2023-04-09 04:59:12 -0700

    Task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,8 +10,19 @@
 
 void print_binary(unsigned long int n)
 {
-	for (int j = sizeof(unsigned long int) * 8 - 1; j >= 0; j--)
-	{
-		putchar((n >> j) & 1 ? '1' : '0');
-	}
+	if(n >> 0)
+	{	
+
+		if (n >> 1)
+		{
+			print_binary(n >> 1);
+			_putchar((n & 1) + '0');
+		}
+	}	
+	else
+	{		
+		_putchar('0');
+	}	
+
 }
+
