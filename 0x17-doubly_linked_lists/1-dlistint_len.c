e062553e7101a864229b80c9dc9a3ae5ec13fb79
Turbo90092 <cliveturbo@gmail.com>
2023-05-18 05:19:40 -0700

    Task1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,15 +8,14 @@
  * Return: the number of elements
  *
  */
-
-size_t dlistint_len(const dlistint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-	count++;
 	h = h->next;
+	count++;
 	}
 	return (count);
 }
